115542fa749664aea377d4420b401163eb7a7451
mB-PiBox <marshallbaer@gmail.com>
2016-08-22 13:00:23 -0500

    AM/PM & Clean Up
    Added AM/PM indication via Left colon top/bottom dots. Cleaned up code, removing lines not needed/leftover from testing.

--- examples/clock.py
+++ examples/clock.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+import os, sys
 
 #	name = "clock.py",
 #   version = "0.0.1",
-#   author = "mB-Pibox",
+#   author = "mB-PiBox",
 #   description = ("A simple clock"),
 #   license = "MIT"
 
@@ -23,26 +24,12 @@ display.begin()
 #Clear the display
 display.clear()
 
-# Keep track of the colon/dots being turned on or off.
-#colon = False
-#leftColon = False
-#leftTop = False
-#leftBottom = False
-#decimal = False
-
 ###############################
 ######## CLOCK CODE ###########
 ###############################
 
 print "Press CTRL+C to exit"
 display.set_brightness(5)
-
-display.set_colon(True)
-
-#display.set_left_colon(True)
-#display.set_fixed_decimal(True)
-#display.set_left_top_dot(True)
-#display.set_left_bottom_dot(True)
 
 # Continually update the time on a 4 char, 7-segment display
 try:
@@ -51,21 +38,27 @@ try:
 		hour = ((now.hour + 11) % 12) + 1
 		minute = now.minute
 		second = now.second
+		ampm = time.strftime("%p")
 		# Set Hours
 		display.set_digit(0, int(hour / 10))     # Tens
 		display.set_digit(1, hour % 10)          # Ones
 		# Set Minutes
 		display.set_digit(2, int(minute / 10))   # Tens
 		display.set_digit(3, minute % 10)        # Ones
-  
-		display.write_display()     # Tens
-		# Wait one second
+		display.set_colon(second % 2)
+		if (ampm == "AM"):
+			display.set_left_bottom_dot(True)
+			display.set_left_top_dot(False)
+		else:
+			display.set_left_top_dot(True)
+			display.set_left_bottom_dot(False)
+		display.write_display()
 		time.sleep(1)
 
 except KeyboardInterrupt:
-	print "\n####################################"
+	print "\n------------------------------------"
 	print "A keyboard interruption was detected"
-	print "####################################"
+	print "------------------------------------"
 
 except:
 	print "An error or exception has occurred!"
